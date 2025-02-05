1d77488c8c6c0589813c98780eafe80bdf55bc24
irinesara <irine.ixa@gmail.com>
2025-02-05 12:37:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const { resolve } = require('path');
+const { connectDB } = require('./db');
 
 const app = express();
-const port = 3010;
 
-app.use(express.static('static'));
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.listen(PORT, async () => {
+    try {
+        await connectDB(MONGO_URI);
+        console.log(`Server running on port ${PORT}`);
+    } 
+    catch (error) { 
+        console.error(error);
+    }
+}); 
+
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    res.send('Hello World');
 });
